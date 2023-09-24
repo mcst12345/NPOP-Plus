423001f86e8bbf7842bfece06081a000977cdc0d
root <herobrinetlb@gmail.com>
2023-09-24 12:32:19 +0800

    commit

---
+++ _fabric/src/main/java/miku/npop/fabric/NPOPPreLaunch.java
@@ -0,0 +1,10 @@
+package miku.npop.fabric;
+
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+
+public class NPOPPreLaunch implements PreLaunchEntrypoint {
+    @Override
+    public void onPreLaunch() {
+
+    }
+}
